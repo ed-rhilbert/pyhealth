d6ec93929e9f55775e31cdcd93b2a64055502c0d
Renan Hilbert <74606040+ed-rhilbert@users.noreply.github.com>
2023-07-12 10:24:02 +0200

    Delete program.py

--- program.py
+++
@@ -1,9 +0,0 @@
-from pyhealth.calculations.calculate import calculate_bmi
-
-
-if __name__ == '__main__':
-    heights = [1.75, 1.65, 1.80]
-    weights = [80, 55, 95.2]
-
-    bmis = calculate_bmi(heights, weights)
-    print(bmis)
